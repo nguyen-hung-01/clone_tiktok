0b834b4ea7a58af047141df30302d41cdeeadc36
hungln <ng.hung01.it@gmail.com>
2024-11-14 17:06:13 +0700

    update folder src

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Fragment } from "react";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/layouts";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/GlobalStyle/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function GlobalStyle({ children }) {
+  return children;
+}
+
+export default GlobalStyle;
